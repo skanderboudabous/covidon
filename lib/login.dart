db18e6453c6573fe7feee2af1ec7ee513cc9598a
skander <skanderdeutschland@gmail.com>
2020-04-03 18:27:19 +0100

    register

---
+++ lib/login.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State createState() => new LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _iconAnimation;
+  AnimationController _iconAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController = new AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 500));
+    _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.bounceOut,
+    );
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      backgroundColor: Colors.white,
+      body: new Stack(fit: StackFit.expand, children: <Widget>[
+        new Image(
+          image: new AssetImage("assets/images/login_back.jpg"),
+          fit: BoxFit.cover,
+          colorBlendMode: BlendMode.darken,
+          color: Colors.black87,
+        ),
+        new Theme(
+          data: new ThemeData(
+              brightness: Brightness.dark,
+              inputDecorationTheme: new InputDecorationTheme(
+                hintStyle: new TextStyle(color: Colors.teal, fontSize: 20.0),
+              )),
+          isMaterialAppTheme: true,
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AnimatedContainer(
+                  duration: new Duration(seconds: 1),
+                  width: _iconAnimationController.value * 150,
+                  height: _iconAnimationController.value * 100,
+                  alignment: Alignment.center,
+                  curve: Curves.decelerate,
+                  child: Image(
+                      image: new AssetImage("assets/images/logoIIT.png"))),
+              new Container(
+                padding: const EdgeInsets.all(30.0),
+                child: new Form(
+                  autovalidate: true,
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'EMAIL',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green))),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'PASSWORD ',
+                            labelStyle: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green))),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 50.0),
+                      Container(
+                        height: 40.0,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(20.0),
+                          shadowColor: Colors.tealAccent,
+                          color: Colors.teal,
+                          elevation: 7.0,
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Center(
+                              child: Text(
+                                'Signin',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.0),
+                      Container(
+                        height: 40.0,
+                        color: Colors.transparent,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.black,
+                                  style: BorderStyle.solid,
+                                  width: 1.0),
+                              color: Colors.teal,
+                              borderRadius: BorderRadius.circular(20.0)),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.of(context).pushNamed('/signup');
+                            },
+                            child: Center(
+                              child: Text('Signup',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat')),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'signup.dart';
+import 'login.dart';
 
 void main() => runApp(new Charity());
 
@@ -6,116 +8,12 @@ class Charity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      routes: <String, WidgetBuilder>{
+        '/signup': (BuildContext context) => new SignupPage()
+      },
       theme: new ThemeData(primarySwatch: Colors.blue),
       home: new LoginPage(),
     );
   }
 }
 
-class LoginPage extends StatefulWidget {
-  @override
-  State createState() => new LoginPageState();
-}
-
-class LoginPageState extends State<LoginPage>
-    with SingleTickerProviderStateMixin {
-  Animation<double> _iconAnimation;
-  AnimationController _iconAnimationController;
-
-  @override
-  void initState() {
-    super.initState();
-    _iconAnimationController = new AnimationController(
-        vsync: this, duration: new Duration(milliseconds: 500));
-    _iconAnimation = new CurvedAnimation(
-      parent: _iconAnimationController,
-      curve: Curves.bounceOut,
-    );
-    _iconAnimation.addListener(() => this.setState(() {}));
-    _iconAnimationController.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.white,
-      body: new Stack(fit: StackFit.expand, children: <Widget>[
-        new Image(
-          image: new AssetImage("assets/images/login_back.jpg"),
-          fit: BoxFit.cover,
-          colorBlendMode: BlendMode.darken,
-          color: Colors.black87,
-        ),
-        new Theme(
-          data: new ThemeData(
-              brightness: Brightness.dark,
-              inputDecorationTheme: new InputDecorationTheme(
-                 hintStyle: new TextStyle(color: Colors.teal, fontSize: 20.0),
-
-              )),
-          isMaterialAppTheme: true,
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              AnimatedContainer(
-                  duration: new Duration(seconds: 5),
-                  width: _iconAnimationController.value * 150,
-                  height: _iconAnimationController.value * 100,
-                  alignment: Alignment.center,
-                  curve: Curves.bounceOut,
-                  child: Image(
-                      image: new AssetImage("assets/images/logoIIT.png")
-                  )
-              ),
-              new Container(
-                padding: const EdgeInsets.all(30.0),
-                child: new Form(
-                  autovalidate: true,
-                  child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                            hintText: " Username or Email", fillColor: Colors.white),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-
-                      new TextFormField(
-                        decoration: new InputDecoration(
-                          hintText: "Password",
-                        ),
-                        obscureText: true,
-                        keyboardType: TextInputType.text,
-                      ),
-                      new Padding(
-                        padding: const EdgeInsets.only(top: 30.0),
-                      ),
-                      new MaterialButton(
-                        height: 50.0,
-                        minWidth: 150.0,
-                        color: Colors.teal,
-                        textColor: Colors.white,
-                        child: new Text("Login"),
-                        onPressed: () {},
-                      ),
-                      new Padding(
-                        padding: const EdgeInsets.only(top: 30.0),
-                      ),
-                      new MaterialButton(
-                          height: 50.0,
-                          minWidth: 150.0,
-                          color: Colors.teal,
-                          textColor: Colors.white,
-                          child: new Text("Register"),
-                          onPressed: () {})
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ]),
-    );
-  }
-}
